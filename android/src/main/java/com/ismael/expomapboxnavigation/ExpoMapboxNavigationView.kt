75a94157a50fc37ed42a696af18150023d8f0204
Ismael <ismael.ousmanem@gmail.com>
2023-09-22 20:50:57 +0100

    Initial commit
    Generated by create-expo-module 0.6.4.

---
+++ android/src/main/java/com/ismael/expomapboxnavigation/ExpoMapboxNavigationView.kt
@@ -0,0 +1,7 @@
+package com.ismael.expomapboxnavigation
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class ExpoMapboxNavigationView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
